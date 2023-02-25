9c24901152cf975dd944b49a835861877039bfad
RyoKitatani <ryo0929m@gmail.com>
2023-02-25 14:33:17 +0900

    feedのフフィールドを追加

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// HackerNewsの1つ1つの投稿
+let links = [
+  {
+    id: "link-0",
+    description: "udemyでgraphqlを学ぶ",
+    url : "https://coeteco.jp/campus"
+  }
+]
+
 // GraphQLスキーマま定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!,
+    description: String!
+    url: String!
   }
 `
 
@@ -11,6 +27,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
+    feed: () => links,
   }
 }
 
